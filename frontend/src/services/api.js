3c22c753babdbd93beab1d3c651bc6750edbff84
Nikhil Kumar <nikhil24382@iiitd.ac.in>
2026-08-15 23:03:27 +0530

    Update API backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "https://scaffoldanalyzer-1.onrender.com",
-
+    baseURL: "https://scaffoldanalyzer-1.onrender.com",
     headers: {
         "Content-Type": "application/json"
     }
